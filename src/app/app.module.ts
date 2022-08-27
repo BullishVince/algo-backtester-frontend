14fb5317f3a79c73f5e9dba19c9195c0af94ba66
BullishVince <vincent@bullishlife.com>
2022-08-27 22:37:57 +0200

    setup application structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MainModule } from 'src/components/main.module';
+import { SharedModule } from 'src/shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    MainModule
   ],
   providers: [],
   bootstrap: [AppComponent]
